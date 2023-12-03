41947ab7bd83bfdea6557458907da5a975f580f7
Shaurya Sinha <shaurya.sinha@protonmail.com>
2023-12-04 01:24:18 +0530

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup></script>
 
 <template>
-  <router-view>
-    
-  </router-view>
+  <router-view> </router-view>
+
+  <footer>
+    <div class="text-center" style="margin-top: 100px">
+      Created by Shaurya Sinha
+    </div>
+  </footer>
 </template>
 
 <style scoped></style>
